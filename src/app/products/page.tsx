eecb1a920986cd76082bdb405a89d331f4cb2c99
Lucas Freitas Vilela <lucasvilelapersonal@gmail.com>
2025-02-21 12:44:49 -0300

    chore:add eslint sort plugin and prettier tailwind plugin

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,12 +1,12 @@
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 const ProductPage = () => {
   return (
-    <div className='p-5 border border-red-500 rounded-xl'>
-      <h1 className='text-red-500'>product page</h1>
+    <div className="rounded-xl border border-red-500 p-5">
+      <h1 className="text-red-500">product page</h1>
       <Button>Click</Button>
-      <Input placeholder='Bora fechar esse projeto' />
+      <Input placeholder="Bora fechar esse projeto" />
     </div>
   );
 };
